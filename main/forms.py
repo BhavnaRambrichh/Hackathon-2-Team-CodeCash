435532ab743c4c63dcbc5039b6dc1b1ca7b934f3
jessandria <jessie.andriamanankasina@gmail.com>
2024-06-09 06:38:39 +0400

    views

---
+++ main/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from django.contrib.auth.models import User
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+    
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+
+class UserLoginForm(forms.Form):
+    username = forms.CharField(max_length=100)
+    password = forms.CharField(widget=forms.PasswordInput)
+
+class TransactionForm(forms.Form):
+    amount = forms.DecimalField(max_digits=10, decimal_places=2)
+    to_account = forms.CharField(max_length=12, required=False)
