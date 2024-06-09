f122703cf7581b3c5c5679ff64436265e10cb019
jessandria <jessie.andriamanankasina@gmail.com>
2024-06-09 08:25:44 +0400

    supabase

--- bank/settings.py
+++ bank/settings.py
@@ -141,5 +141,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_URL = 'main/static/css'
-STATICFILES_DIRS = [ STATIC_URL / 'static']
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
