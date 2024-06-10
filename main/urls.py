7dd31cd0ca221a6bea0efead7734fe97f15b1286
jessandria <jessie.andriamanankasina@gmail.com>
2024-06-10 16:31:49 +0400

    urls

--- main/urls.py
+++ main/urls.py
@@ -4,7 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.welcome, name='welcome'),
     path('login_signup/', views.login_signup, name='login_signup'),
-    path('login/', views.login_view, name='login'),
     path('profile/', views.profile, name='profile'),
     path('wallet/', views.wallet, name='wallet'),
     path('forgot_password/', views.forgot_password, name='forgot_password'),
