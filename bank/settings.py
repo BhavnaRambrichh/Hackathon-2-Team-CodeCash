42a95dacbca7cbddef0767b8e63963c38289b937
BhavnaRambrichh <triscia.rambrichh@outlook.com>
2024-06-11 14:33:37 +0400

    Inserted Pictures

--- bank/settings.py
+++ bank/settings.py
@@ -17,6 +17,9 @@ from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 
 # Quick-start development settings - unsuitable for production
@@ -136,7 +139,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- main/static/js/login_signup.js
+++ main/static/js/login_signup.js
@@ -1,11 +1,23 @@
-const registerButton = document.getElementById("register");
-const loginButton = document.getElementById("login");
-const container = document.getElementById("container");
+document.addEventListener('DOMContentLoaded', (event) => {
+    const container = document.getElementById('container');
+    const loginButton = document.getElementById('login');
+    const registerButton = document.getElementById('register');
+    const registerBtn = document.getElementById('registerBtn');
+    const loginBtn = document.getElementById('loginBtn');
 
-registerButton.addEventListener("click", () => {
-    container.classList.add("right-panel-active");
-});
+    loginButton.addEventListener('click', () => {
+        container.classList.remove('right-panel-active');
+    });
 
-loginButton.addEventListener("click", () => {
-    container.classList.remove("right-panel-active");
+    registerButton.addEventListener('click', () => {
+        container.classList.add('right-panel-active');
+    });
+
+    registerBtn.addEventListener('click', () => {
+        
+    });
+
+    loginBtn.addEventListener('click', () => {
+        
+    });
 });
