5ab8e06e1014a43f8a23d57e3cd6a36d944a6643
BhavnaRambrichh <triscia.rambrichh@outlook.com>
2024-06-12 11:43:15 +0400

    updated to static

--- static/js/login_signup.js
+++ static/js/login_signup.js
@@ -1,23 +1,11 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    const container = document.getElementById('container');
-    const loginButton = document.getElementById('login');
-    const registerButton = document.getElementById('register');
-    const registerBtn = document.getElementById('registerBtn');
-    const loginBtn = document.getElementById('loginBtn');
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
 
-    loginButton.addEventListener('click', () => {
-        container.classList.remove('right-panel-active');
-    });
+signUpButton.addEventListener('click', () => {
+    container.classList.add('right-panel-active');
+});
 
-    registerButton.addEventListener('click', () => {
-        container.classList.add('right-panel-active');
-    });
-    registerBtn.addEventListener('click', () => {
-        
-    });
-
-    loginBtn.addEventListener('click', () => {
-        
-    });
-
+signInButton.addEventListener('click', () => {
+    container.classList.remove('right-panel-active');
 });
